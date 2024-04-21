5bd8f12fd6b709ad0f73665a0a9479136344a261
Sampsa Ollikainen <sampsa.ollikainen@gmail.com>
2024-04-21 22:52:33 +0300

    Revert "This is a mistake commit"
    This reverts commit 0caced320d2aea663938a8a6dac240962460e66a.

--- messages.py
+++ messages.py
@@ -3,7 +3,7 @@ import users
 from sqlalchemy.sql import text
 
 def get_messages(chain_id):
-  sql = text("SELECT M.content, U.username, COUNT(L.id), M.id FROM messages M, users U, likes L WHERE M.user_id=U.id AND M.chain_id=:chain_id AND L.message_id=M.id GROUP BY M.id order by M.id")
+  sql = text("SELECT M.content, U.username FROM messages M, users U WHERE M.user_id=U.id AND M.chain_id=:chain_id order by M.id")
   result = db.session.execute(sql, {"chain_id":chain_id})
   return result.fetchall()
 
@@ -13,14 +13,5 @@ def send(content, chain_id):
     return False
   sql = text("INSERT INTO messages (content, chain_id, user_id) VALUES (:content, :chain_id, :user_id)")
   db.session.execute(sql, {"content":content, "chain_id":chain_id, "user_id":user_id})
-  db.session.commit()
-  return True
-
-def like(message_id):
-  user_id = users.user_id()
-  if user_id == 0:
-    return False
-  sql = text("INSERT INTO likes (message_id, user_id) VALUES (:message_id, :user_id)")
-  db.session.execute(sql, {"message_id":message_id, "user_id":user_id})
   db.session.commit()
   return True
